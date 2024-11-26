23e82a8d7c174aeb7be2741ede19e433e9b3f344
TomonoriMiura <neko.sakura87nya@gmail.com>
2024-11-26 16:16:12 +0900

    11/26制作

--- static/javascript/QR.js
+++ static/javascript/QR.js
@@ -2,3 +2,28 @@ src="https://code.jquery.com/jquery-3.6.0.min.js"
 integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
 crossorigin="anonymous"
 
+let formID = document.getElementById('studentID');
+let keyword = document.getElementById('keyword');
+
+function buttonClick(){
+    // Ajax通信を開始
+    $.ajax({
+      url: '',
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        'studentID':formID.value,
+        'keyword': keyword.value,
+      }
+      //timeout: 5000,
+    })
+    .done(function(data) {
+        // 通信成功時の処理を記述
+        alert("success!");
+    })
+    .fail(function() {
+        // 通信失敗時の処理を記述
+        alert('error!');
+    });
+  }
+    
